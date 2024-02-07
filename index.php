7916aaad9ae69f630d072eee0cfb8b4b9997ffec
Guilherme Faria <guilhermefaria321@gmail.com>
2024-02-06 21:20:18 -0300

    ok
    atualização na pagina index, login e senha compatíveis com o banco de dados.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Empresa</title>
-    <link rel="stylesheet" href="restrito/style.css">
+    <link rel="stylesheet" href="restrita/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
@@ -28,25 +28,41 @@
                         </div>
                         <div class="mb-3 form-check">
                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Confirmar os dados.</label>
+                            <label class="form-check-label" for="exampleCheck1">Lembrar dados</label>
                         </div>
                         <button type="submit" class="btn btn-primary">Acessar</button>
                     </form>
-                    <?php 
-                        if(isset($_POST["login"])){
-                            $login = $_POST["login"];
-                            $senha = $_POST["senha"];
-                            if(($logion == "admin") and ($senha == "admin")){
-                                header("location: restrito");
-                            }else {
-                                echo "login invalido!";
+                    <?php
+                    if (isset($_POST["login"])) {
+                        $login = $_POST["login"];
+                        $senha = md5($_POST["senha"]);
+
+                        include "restrito/conexao.php";
+                        $sql = "SELECT * FROM `usuarios` WHERE login = '$login' AND senha = '$senha'";
+
+                        if ($result = mysqli_query($conn, $sql)) {
+                            $num_registros = mysqli_num_rows($result);
+                            if ($num_registros == 1) {
+                                $linha = mysqli_fetch_assoc($result);
+                                if (($login == $linha["login"]) and ($senha == $linha["senha"])) {
+                                    session_start();
+                                    $_SESSION["user"] = "Gui";
+                                    header("location: restrito");
+                                } else {
+                                    echo "login invalido!";
+                                }
+                            } else {
+                                echo "Login ou senha inválidos ou não encontrados!";
                             }
+                        }else{
+                            echo "Nenhum resultado do banco de dados!";
                         }
+                    }
                     ?>
                 </div>
             </div>
-        </div>      
-        <div class="col-3"></div> 
+        </div>
+        <div class="col-3"></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
